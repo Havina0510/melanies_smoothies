957e6c592f12249dcce05ae1764c98ff6661a7bb
Havina0510 <havina0220@gmail.com>
2024-10-18 10:19:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,8 @@ ingredients_list = st.multiselect(
     ,my_dataframe
     ,max_selections=5
 )
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
